a961308b2e68b063a700f44d07a71b5b5530e3f4
root <root@ip-172-31-7-40.ap-south-1.compute.internal>
2022-12-17 09:49:32 +0000

    change message

---
+++ open.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req, res) => res.send('Devops_Project'));
+
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
+
+
+
+
+
+
